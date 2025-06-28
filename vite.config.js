3124825268e9af17e02701b387c68ff63600db20
woodid012 <dbwooding88@gmail.com>
2025-06-28 13:07:15 +1000

    template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vite-Tailwind-React-Portfolio2/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
 })
